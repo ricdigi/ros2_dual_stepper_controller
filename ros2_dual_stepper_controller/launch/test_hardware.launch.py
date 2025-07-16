da71ebcd0c9e590b644b10b4635f1a5667010fcc
ricdigi <riccardodigirolamo01@gmail.com>
2025-07-16 23:15:58 +0200

    corrected lidar launch

--- ros2_dual_stepper_controller/launch/test_hardware.launch.py
+++ ros2_dual_stepper_controller/launch/test_hardware.launch.py
@@ -1,11 +1,12 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
 
 
 def generate_launch_description():
